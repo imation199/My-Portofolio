b2a29c2e31cb1a7720455f5d17a1f49d19a59e4c
George Cristian Limba <cristi201199@gmail.com>
2021-03-01 10:09:54 +0200

    Home page fix text

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
         <div>
             <img src={image} alt="Sky Diving" className="absolute object-cover w-full h-full" />
             <section className="relative flex justyfi-center min-h-screen pt-12 lg: pt-64 px-8" >
-                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name"  > Aloha. I`m George. </h1>
+                <h1 className="lg:text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug lg:px-20 home-name "  > Aloha. I`m George. </h1>
             </section>
         </div>
     )
